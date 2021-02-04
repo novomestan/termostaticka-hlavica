538380649e88b9a8ab7581c4a803689d67cfff55
novomestan <78145236+novomestan@users.noreply.github.com>
2021-02-04 21:05:59 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -12,6 +12,13 @@ basic.pause(1000)
 wuKong.setServoAngel(wuKong.ServoList.S0, 90)
 let Merana = 0
 Ziadana = 25
+I2C_LCD1602.ShowString("Merana  T:", 0, 0)
+I2C_LCD1602.ShowString("Ziadana T:", 0, 1)
 basic.forever(function () {
-	
+    Merana = input.temperature()
+    I2C_LCD1602.ShowNumber(Merana, 11, 0)
+    I2C_LCD1602.ShowNumber(Ziadana, 11, 1)
+    basic.pause(500)
+    I2C_LCD1602.ShowString("    ", 11, 0)
+    I2C_LCD1602.ShowString("    ", 11, 1)
 })
