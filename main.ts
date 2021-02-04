cd8d98ad13af99159ecfc2b77f5eba13590955cb
novomestan <78145236+novomestan@users.noreply.github.com>
2021-02-04 21:47:23 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
 input.onButtonPressed(Button.A, function () {
     Ziadana = Ziadana + 0.1
+    ZobrazZT()
 })
+function ZobrazZT () {
+    I2C_LCD1602.ShowString("    ", 11, 1)
+    I2C_LCD1602.ShowNumber(Ziadana, 11, 1)
+}
 input.onButtonPressed(Button.B, function () {
     Ziadana = Ziadana - 0.1
+    ZobrazZT()
 })
+let Merana = 0
 let Ziadana = 0
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.clear()
@@ -11,14 +18,10 @@ wuKong.setServoAngel(wuKong.ServoList.S0, 0)
 basic.pause(1000)
 wuKong.setServoAngel(wuKong.ServoList.S0, 90)
 wuKong.stopAllMotor()
-let Merana = 0
 Ziadana = 25
 I2C_LCD1602.ShowString("Merana  T:", 0, 0)
 I2C_LCD1602.ShowString("Ziadana T:", 0, 1)
 basic.forever(function () {
     Merana = input.temperature()
     I2C_LCD1602.ShowNumber(Merana, 11, 0)
-    I2C_LCD1602.ShowString("    ", 11, 1)
-    I2C_LCD1602.ShowNumber(Ziadana, 11, 1)
-    basic.pause(1000)
 })
