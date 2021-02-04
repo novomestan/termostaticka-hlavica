e57635f1058902ddb2f8ff6fef2d192b654af61c
novomestan <78145236+novomestan@users.noreply.github.com>
2021-02-04 20:34:00 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,12 @@
-wuKong.setServoAngel(wuKong.ServoList.S0, 0)
-basic.pause(1000)
-wuKong.setServoAngel(wuKong.ServoList.S0, 180)
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.clear()
-I2C_LCD1602.ShowString("Merana  T:", 0, 0)
-I2C_LCD1602.ShowString("Ziadana T:", 0, 1)
-I2C_LCD1602.ShowNumber(input.temperature(), 11, 0)
-I2C_LCD1602.ShowNumber(25, 11, 1)
+wuKong.setServoAngel(wuKong.ServoList.S0, 0)
+let Merana = 0
+let Ziadana = 0
+basic.forever(function () {
+    Merana = input.temperature()
+    I2C_LCD1602.ShowString("Merana  T:", 0, 0)
+    I2C_LCD1602.ShowString("Ziadana T:", 0, 1)
+    I2C_LCD1602.ShowNumber(Merana, 11, 0)
+    I2C_LCD1602.ShowNumber(Ziadana, 11, 1)
+})
