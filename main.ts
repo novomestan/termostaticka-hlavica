e941f20c498c1d846c6ae4a345b4552d7df6e738
novomestan <78145236+novomestan@users.noreply.github.com>
2021-02-04 21:26:37 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,8 @@
 input.onButtonPressed(Button.A, function () {
-    Ziadana = Ziadana + 0.5
+    Ziadana = Ziadana + 0.1
 })
 input.onButtonPressed(Button.B, function () {
-    Ziadana = Ziadana - 0.5
+    Ziadana = Ziadana - 0.1
 })
 let Ziadana = 0
 I2C_LCD1602.LcdInit(39)
@@ -16,9 +16,8 @@ I2C_LCD1602.ShowString("Merana  T:", 0, 0)
 I2C_LCD1602.ShowString("Ziadana T:", 0, 1)
 basic.forever(function () {
     Merana = input.temperature()
-    I2C_LCD1602.ShowString("    ", 11, 0)
     I2C_LCD1602.ShowNumber(Merana, 11, 0)
     I2C_LCD1602.ShowString("    ", 11, 1)
     I2C_LCD1602.ShowNumber(Ziadana, 11, 1)
-    basic.pause(500)
+    basic.pause(1000)
 })
