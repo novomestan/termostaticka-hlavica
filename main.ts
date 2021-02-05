b36f7e0f59822965c06245c48bbe75dea56e67f3
novomestan <78145236+novomestan@users.noreply.github.com>
2021-02-05 17:18:46 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,10 +8,10 @@ let Merana = 0
 let Ziadana = 0
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.clear()
-wuKong.setServoAngel(wuKong.ServoList.S0, 0)
+wuKong.setServoAngel(wuKong.ServoList.S1, 0)
 basic.pause(500)
 let Servo = 180 / 1.5
-wuKong.setServoAngel(wuKong.ServoList.S0, Servo)
+wuKong.setServoAngel(wuKong.ServoList.S1, Servo)
 Ziadana = 25
 I2C_LCD1602.ShowString("Merana  T:", 0, 0)
 I2C_LCD1602.ShowString("Ziadana T:", 0, 1)
